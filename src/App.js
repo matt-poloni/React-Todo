8ca3a8a34b313c302aadbe3ad1a8d1b6fc788706
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-20 15:40:33 -0400

    Sets up parts of initial structures

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends Component {
   // you will need a place to store your state in this component.
@@ -6,13 +8,20 @@ class App extends Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      listItems: [],
+    }
   }
+
+  handleAddItem = e => {}
+  handleClearCompleted = e => {}
 
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP</h2>
+        <TodoList />
+        <TodoForm />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TodoForm = props => {}
+
+export default TodoForm;
