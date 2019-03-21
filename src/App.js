82397fe98c14fd03d990c9e87899608ae28bdcfe
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-21 18:58:52 -0400

    Adds some minimal styling

--- src/App.js
+++ src/App.js
@@ -13,14 +13,62 @@ class App extends Component {
     this.state = {
       listItems: [
         {
-          task: 'Prepopulated, Complete',
+          task: 'Sprint Challenge - React Wars',
           id: Date.now(),
+          completed: false,
+          filtered: false,
+        },
+        {
+          task: 'Sprint Challenge - Lambda Times, React',
+          id: Date.now() + 1,
+          completed: false,
+          filtered: false,
+        },
+        {
+          task: 'Functional Components I',
+          id: Date.now() + 2,
           completed: true,
           filtered: false,
         },
         {
-          task: 'Prepopulated, Not Complete',
-          id: Date.now() + 1,
+          task: 'Functional Components II',
+          id: Date.now() + 3,
+          completed: true,
+          filtered: false,
+        },
+        {
+          task: 'Class Components I',
+          id: Date.now() + 4,
+          completed: true,
+          filtered: false,
+        },
+        {
+          task: 'Class Components II',
+          id: Date.now() + 5,
+          completed: true,
+          filtered: false,
+        },
+        {
+          task: 'React Tooling',
+          id: Date.now() + 6,
+          completed: false,
+          filtered: false,
+        },
+        {
+          task: 'The React Lifecycle',
+          id: Date.now() + 7,
+          completed: false,
+          filtered: false,
+        },
+        {
+          task: 'React Composition Patterns',
+          id: Date.now() + 8,
+          completed: false,
+          filtered: false,
+        },
+        {
+          task: 'CSS in JS',
+          id: Date.now() + 9,
           completed: false,
           filtered: false,
         },
